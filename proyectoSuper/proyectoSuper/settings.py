4d7f9a8abb7dd9967b9a7c79dc158f73a87334af
pablotebb <4356464+pablotebb@users.noreply.github.com>
2024-06-09 12:29:45 +0100

    Listado super 4

--- proyectoSuper/proyectoSuper/settings.py
+++ proyectoSuper/proyectoSuper/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'appSuper',
+    'appSuper.apps.AppsuperConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
